2181dd3fb692292f36116486aa7380260f667042
retinalfetish <rogue@erratum.com>
2025-03-28 21:07:07 -0400

    Added emotion scores

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
@@ -6,5 +7,8 @@ def emotion_detector(text_to_analyze):
     input_json = { "raw_document": { "text": text_to_analyze } }
 
     response = requests.post(url, headers = headers, json = input_json)
+    formatted_response = json.loads(response.text)
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    emotions["dominant_emotion"] = max(emotions, key = emotions.get)
 
-    return response.text
+    return emotions
