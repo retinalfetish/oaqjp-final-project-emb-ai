402ab080ee43aafba9bf14180fc7fcad0f1b2889
retinalfetish <rogue@erratum.com>
2025-03-28 22:49:10 -0400

    Added Flask server

---
+++ server.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template
+from EmotionDetection.emotion_detection import emotion_detector
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def detector():
+    text = request.args.get("textToAnalyze")
+    result = emotion_detector(text)
+
+    keys = list(result.keys())
+    values = list(result.values())
+
+    response = (
+        f"For the given statement, the system response is "
+        f"'{keys[0]}': {values[0]}, "
+        f"'{keys[1]}': {values[1]}, "
+        f"'{keys[2]}': {values[2]}, "
+        f"'{keys[3]}': {values[3]}, "
+        f"and '{keys[4]}': {values[4]}. "
+        f"The dominant emotion is {result['dominant_emotion']}."
+    )
+
+    # for k, v in result.items():
+    #     response = response + f"'{k}': {v}, "
+    
+    return response
+
+@app.route("/")
+def index():
+    return render_template("index.html")
